6031206d0d4f1822861f5fd5eaa1132b2678d5df
Annsysadm <50216873+Annsysadm@users.noreply.github.com>
2019-06-17 14:22:50 +0300

    Task_4_6 updated

--- 4/4.6.py
+++ 4/4.6.py
@@ -12,34 +12,16 @@ Outbound Interface     FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
-ospf_route = ospf_route.replace('[', ' ') # убираем лишние квадратные скобки
-ospf_route = ospf_route.replace(']', ' ') # убираем лишние квадратные скобки
-b = ospf_route.split(',') # получился список
-print('b =', b)
-e = b[1:3]
-g = b[-1].strip()
-g = g.split()
-h = b[-2].strip()
-h = h.split()
-print('e =', e)
-print('g =', g)
-print('h =', h)
-c = b[0].split() # нулевой элемент списка b перевели в список
-c.pop(3)         #удалили 3 элемент из списка с
-print('c =', c)
-f = c + h + g  # результирующий список
-print('f =', f)
-print(type(f))
-f_str = tuple(f)
-print('f =', f)
-print(type(f_str))
+oospf_route = ospf_route.replace('[', '').replace(']', '').replace(',', '')
+# убираем лишние квадратные скобки и лишние запятые
+f = ospf_route.split() # получился список
 
 ip_template = '''
-Protocol:              {0:}
-Prefix:                {1:}
-AD/Metric:             {2:}
-Next-Hop:              {3:}
-Last update:           {4:}
-Outbound Interface     {5:}
+Protocol:              {}
+Prefix:                {}
+AD/Metric:             {}
+Next-Hop:              {}
+Last update:           {}
+Outbound Interface     {}
 '''
-print(ip_template.format('O', '10.0.24.0/24', '110/41', '10.0.13.3', '3d18h', 'FastEthernet0/0'))
+print(ip_template.format('OSPF', f[1], f[2], f[4], f[5], f[6]))
