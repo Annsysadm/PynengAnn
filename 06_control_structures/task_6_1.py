ee1f0394812fd9b13839d1848e8bea4e3d9d94b7
Annsysadm <50216873+Annsysadm@users.noreply.github.com>
2019-06-20 15:14:20 +0300

    Task_6_1_ready

--- 06_control_structures/task_6_1.py
+++ 06_control_structures/task_6_1.py
@@ -12,4 +12,7 @@
 '''
 
 mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
-
+mac_cisco = []
+for address in mac:
+    mac_cisco.append(address.replace(':', '.').upper())
+print(mac_cisco)
