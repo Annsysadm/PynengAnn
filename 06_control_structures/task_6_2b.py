886262fa2c52eaccbc0749700d2dc0a1e874e5eb
Annsysadm <50216873+Annsysadm@users.noreply.github.com>
2019-06-25 12:58:29 +0300

    task_6_2b preliminary solving
    1st v.  but it doesn't work properly

--- 06_control_structures/task_6_2b.py
+++ 06_control_structures/task_6_2b.py
@@ -9,3 +9,33 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+input_ip_address = input('Введите ip-address: ')
+list_input_address = (input_ip_address.split('.'))
+oct1, oct2, oct3, oct4 = int(list_input_address[0]), int(list_input_address[1]), int(list_input_address[2]), int(list_input_address[3])
+
+ip_correct = False
+while not ip_correct:
+    if not list_input_address[0].isdigit() and list_input_address[1].isdigit() and list_input_address[2].isdigit() and list_input_address[3].isdigit():
+        print('Неправильный ip-адрес (не состоит из цифр)\n')
+        input_ip_address = input('Введите ip-address снова: ')
+    elif len(input_ip_address.split('.')) != 4:
+        print('Неправильный ip-адрес - не состоит из 4 элементов разделенных точкой\n')
+        input_ip_address = input('Введите ip-address снова: ')
+    elif not (oct1 in range(256) and  oct2 in range(256) and  oct3 in range(256) and oct4 in range(256)):
+        print('Attention! Неправильный ip-адрес.\n')
+        input_ip_address = input('Введите ip-address снова: ')
+    else:
+        ip_correct = True
+'''
+    if ip_address[0] in range(1, 224):
+        result = 'unicast'
+    elif ip_address[0] in range(224, 239):
+        result = 'multicast'
+    elif ip_address == [255, 255, 255, 255]:
+        result = 'local broadcast'
+    elif ip_address == [0, 0, 0, 0]:
+        result = 'unassigned'
+    else:
+        result = 'unused'
+'''
