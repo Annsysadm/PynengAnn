8349785cab29f1843298f647ed2c0da944efc32d
Annsysadm <50216873+Annsysadm@users.noreply.github.com>
2019-06-26 12:10:24 +0300

    Task_7_2a done

--- 07_files/task_7_2a.py
+++ 07_files/task_7_2a.py
@@ -11,5 +11,14 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+#!/usr/bin/env python
+
+from sys import argv
+file_name = argv[1]
 
 ignore = ['duplex', 'alias', 'Current configuration']
+with open(file_name) as f:
+    for line in f:
+        for command in ignore:
+            if not line.startswith('!') and not (set(ignore) & set(line.split())):
+                print(line, end='')
