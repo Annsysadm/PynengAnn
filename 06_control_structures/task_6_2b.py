25c7c153bacc6eeb1dea906731ed504566314c54
Annsysadm <50216873+Annsysadm@users.noreply.github.com>
2019-06-26 12:26:47 +0300

    Task_6_2b updated
    using List comprehensions
    ip_address = [int(octet) for octet in input_ip_address if octet.isdigit() and int(octet) in range(256)]

--- 06_control_structures/task_6_2b.py
+++ 06_control_structures/task_6_2b.py
@@ -29,6 +29,21 @@ while not ip_correct:
     else:
         input_ip_address = input('Введите ip-address еще раз: ').split('.')
 
+# Проверка правильности ввода ip-адреса
+'''while not ip_correct:
+    if len(input_ip_address) != 4:
+        print('Неправильный ip-адрес - не состоит из 4 элементов разделенных точкой')
+    if (len(input_ip_address)) == 4:
+        ip_address = [int(octet) for octet in input_ip_address if octet.isdigit() and int(octet) in range(256)]
+        if len(ip_address) == 4:
+            ip_correct = True
+        else:
+            print('Неправильный ip-адрес')
+            input_ip_address = input('Введите ip-address еще раз: ').split('.')
+    else:
+        input_ip_address = input('Введите ip-address еще раз: ').split('.')
+'''
+# Классификация ip-адреса
 if ip_address[0] in range(1, 224):
     result = 'unicast'
 elif ip_address[0] in range(224, 239):
