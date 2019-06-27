571cb7d73185ef7c92e97826a60a87f23285c352
Annsysadm <50216873+Annsysadm@users.noreply.github.com>
2019-06-27 11:19:08 +0300

    Task_7_3a sorted
    but I need to find how to output correctly

--- 07_files/task_7_3a.py
+++ 07_files/task_7_3a.py
@@ -22,3 +22,17 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+#!/usr/bin/env python
+line_list = []
+with open('CAM_table.txt') as srs:
+    for line in srs:
+        if not line.startswith('-') and not line.startswith('\n') and line.split()[0].isdigit():
+            vlan, mac, _, intf = line.split()
+            line_list.append([int(vlan), [mac, intf]])
+            result = sorted(line_list)
+
+            #result = sorted([vlan, mac, intf])
+            #print(result)
+            #print(f'{vlan:8}{mac:17}{intf:8}')
+    #print(result)
+print(str(result))
